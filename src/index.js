a05532ac273d02e4c54d39b4e0ec13a5d699b94e
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2018-02-25 13:26:11 +0300

    100prc

--- src/index.js
+++ src/index.js
@@ -1,30 +1,61 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = [];
+
+    this.compareFunction = function (a, b) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+
+      return 0;
+    }
   }
 
   add(element) {
     // your implementation
+    return this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+
+    var sortArray = [];
+
+    this.array.forEach((value, index) => {
+      for (let i = 0; i < indices.length; i++) {
+        if (indices[i] === index) sortArray.push(value);
+      }
+    })
+
+    if (this.compareFunction) {
+      sortArray.sort(this.compareFunction);
+    }
+
+    this.array.forEach((value, index) => {
+      for (let i = 0; i < indices.length; i++) {
+        if (indices[i] === index) this.array[index] = sortArray.shift();
+      }
+    })
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
